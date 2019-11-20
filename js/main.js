e4a634d561a6f1fbda7d9fe8cbfac0c1a165da10
DEN <voland2114@gmail.com>
2019-11-20 23:24:44 +0700

    add media

--- js/main.js
+++ js/main.js
@@ -1,20 +1,53 @@
 
 
- $(function(){
+$(function () {
 
    $('.slider-list').slick({
       slidesToShow: 1,
       fade: true,
-      arrow:true,
+      arrows: true,
       asNavFor: '.slider-nav'
-    });
-   
-   
-    $('.slider-nav').slick({
+   });
+
+
+   $('.slider-nav').slick({
       slidesToShow: 6,
       asNavFor: '.slider-list',
       dots: false,
       focusOnSelect: true,
-      variableWidth: true,
-    });
- });
+      // arrows: false,
+      // variableWidth: true,
+      responsive: [
+         {
+            breakpoint: 961,
+            settings: {
+               slidesToShow: 5,
+            }
+         },
+         {
+            breakpoint: 740,
+            settings: {
+               slidesToShow: 4,
+            }
+         },
+         {
+            breakpoint: 601,
+            settings: {
+               slidesToShow: 3,
+            }
+         },
+         {
+            breakpoint: 441,
+            settings: {
+               slidesToShow: 2,
+            }
+         },
+      ]
+   });
+
+
+   $('.burger-menu').on('click', function () {
+      $('.header__menu > ul').slideToggle();
+      $(this).toggleClass('header__menu-active');
+   });
+});
